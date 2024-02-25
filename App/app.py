036f392065b0345a385467b7b6b5a860bc98d452
SamH477 <huhns@etown.edu>
2024-02-25 18:07:43 -0500

    Start developing UI

---
+++ App/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from PyPDF2 import PdfReader
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+# Define a route for uploading a pdf file
+@app.route('/pdf_upload')
+def pdf_upload():
+    return render_template('pdf_upload.html')
+
+# Define a route for processing pdf files
+@app.route('/upload', methods = ["POST"])
+def process_pdf():
+    pdf_file = request.files['file']
+    reader = PdfReader(pdf_file)
+    page = reader.pages[0]
+    text = page.extract_text()
+    
+    return render_template('process_pdf.html', text=text)
+
+# Define a route so users can see their flashcards
+@app.route('/my_flashcards')
+def my_flashcards():
+    return render_template('my_flashcards.html')
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
--- api_config/openai-test.py
+++ api_config/openai-test.py
@@ -1,14 +1,18 @@
 from openai import OpenAI
 from api_config import OPENAI_API_KEY
+from PyPDF2 import PdfReader
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-completion = client.chat.completions.create(
+'''completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
   ]
-)
+) '''
 
-print(completion.choices[0].message)
+reader = PdfReader("HW1.pdf")
+page = reader.pages[0]
+print(page.extract_text())
+#print(completion.choices[0].message)
